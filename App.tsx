874e0396104054f90caf3c5a534ac17ab789c0b2
osmanforhad <osmanforhad93@gmail.com>
2025-06-24 12:57:49 +0600

    working with Yup schema validation

--- App.tsx
+++ App.tsx
@@ -1,6 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
+//Form Validation
+import * as Yup from 'yup';
+//writing schema for password
+const PasswordSchema = Yup.object().shape({
+  passwordLength: Yup.number()
+    .min(4, 'password should be minum 4 char')
+    .max(16, 'password should be max 16 char')
+    .required('password is required')
+});
+
 export default function App() {
   return (
     <View>
