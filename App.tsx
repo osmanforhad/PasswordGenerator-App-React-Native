a2c9184e9658cf22c3844f9044904ba2c879f20b
osmanforhad <osmanforhad93@gmail.com>
2025-06-24 16:19:20 +0600

    write locgic for create or generate password

--- App.tsx
+++ App.tsx
@@ -35,7 +35,12 @@ export default function App() {
    * @param passwordLength 
    */
   const createPassword = (characters: string, passwordLength: number) => {
-    //
+    let result = '';
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length);
+      result += characters.charAt(characterIndex);
+    }
+    return result;
   }
 
   /**
